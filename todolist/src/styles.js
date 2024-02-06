d13336b106eb880b79c3b2990fe801ee930af010
tvaditya <tvaditya@gmail.com>
2024-02-06 16:09:08 -0300

    First

---
+++ todolist/src/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    padding: 10px;
+    background-color: black;
+    `
+export const Titulo = styled.h1`
+    background-color: black;
+    color: orange;
+    `
