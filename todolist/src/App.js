0b0bd6114f25330b5032eccd8392221beece8f21
tvaditya <tvaditya@gmail.com>
2024-02-12 11:54:16 -0300

    useless props that don't work

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,41 @@
 import TodoListImg from './assets/logo.png';
 import {Titulo, Container, MainContainer,
     LeftContainer, RightContainer, LogoImage,
-    SubHeadLine, HeadLine} from './styles';
+    SubHeadLine, HeadLine, SubTitulo, Tarefa, TarefaInfo} from './styles';
+import {useState, useRef, useEffect} from "react";
+import axios from "axios";
 
 
-function App() {
+
+export default function App() {
+  // const tasks = [{id:Date.now() + '_' + Math.random(),text: "Tarefa anterior"},
+  //     {id:Date.now() + '_' + Math.random(), text: "Tarefa nova"}]
+  const [tasks, setTasks] = useState([]);
+
+  const taskText = useRef("");
+
+  async function removeTask(id) {
+        console.log("Removendo tarefa...")
+        await axios.delete(`https://plataforma.universodev.com.br/api/todolist/${id}`)
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+    useEffect(() => {
+      async function listTasks() {
+        const {data: taskList} = await axios.get('https://plataforma.universodev.com.br/api/todolist/')
+        setTasks(taskList);
+      }
+      listTasks();
+    }, []);
+  async function insertNewtask(event) {
+    if (event.key === 'Enter') {
+      const {data: newTaskInfo} = await axios.put('https://plataforma.universodev.com.br/api/todolist/',
+          {text: taskText.current.value})
+      // setTasks([...tasks, {id:Date.now() + '_' + Math.random(), text: taskText.current.value}]);
+        setTasks([...tasks, {id: newTaskInfo.id, text: newTaskInfo.text}]);
+      taskText.current.value = "";
+    }
+  }
+
   return (
     <MainContainer>
         <Container>
@@ -16,11 +47,22 @@ function App() {
                 <SubHeadLine>Adicione ao lado as tarefas que deseja fazer hoje e não pare até ela estar vazia!</SubHeadLine>
             </LeftContainer>
             <RightContainer>
-                Lado Direito
+                <Titulo>Minhas Tarefas</Titulo>
+                <SubTitulo>Tarefas do dia...</SubTitulo>
+
+                {tasks.map((task) => (
+                    <Tarefa id={task.id} text={task.text} updateTasks={setTasks} />
+                ))}
+
+                <TarefaInfo input type={"text"}
+                            placeholder={"Adicione a próxima tarefa..."}
+                            onKeyDown = {insertNewtask}
+                            ref={taskText}
+                />
             </RightContainer>
         </Container>
     </MainContainer>
   );
 }
 
-export default App;
+// export default App;
--- todolist/src/styles.js
+++ todolist/src/styles.js
@@ -9,6 +9,7 @@ export const MainContainer = styled.div`
 export const Container = styled.div`
     display: flex;
     flex-direction: row;
+    align-items: center;
     //background-color: black;
     `
 export const LeftContainer = styled.div`
@@ -18,6 +19,23 @@ export const LeftContainer = styled.div`
     //background-color: yellow;
     `
 
+export const RightContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    //background-color: green;
+    width: 500px;
+    height: fit-content;
+    margin-left: 52px;
+    padding: 45px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+
+    border-radius: 45px;
+    background: #FFF;
+    box-shadow: 0px 40px 100px 0px rgba(0, 0, 0, 0.08), 0px 10px 80px 0px rgba(0, 0, 0, 0.06), 
+    0px 2px 15px 0px rgba(0, 0, 0, 0.12), 0px 2px 4px 0px rgba(0, 0, 0, 0.06);
+    `
 export const HeadLine = styled.div`
     display: flex;
     align-items: center;
@@ -26,7 +44,6 @@ export const HeadLine = styled.div`
         color: #1F5FA3
     }
     `
-
 export const SubHeadLine = styled.p`
     font-size: 24px;
     font-style: normal;
@@ -34,12 +51,6 @@ export const SubHeadLine = styled.p`
     line-height: normal;
     margin-top: 16px;
     `
-export const RightContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: green;
-    `
-
 export const LogoImage = styled.img`
     margin-right: 8px;
     `
@@ -51,4 +62,55 @@ export const Titulo = styled.h1`
     font-style: normal;
     font-weight: 800;
     line-height: normal;
+    `
+
+export const SubTitulo = styled.p`
+    color: rgba(53, 56, 62, 0.50);
+    font-size: 20px;
+    font-weight: 700;
+    //line-weight: normal;
+    `
+
+export const Tarefa = styled.div`
+    display: flex;
+    width: 410px;
+    padding: 15px;
+    align-items: center;
+    gap: 15px;
+    //border-radius: 11.25px;
+    //background: rgba(53, 56, 62, 0.05);
+    
+    border-radius: 22.5px;
+    background: rgba(53, 56, 62, 0.05);
+    
+    svg{
+        background: rgba(53, 56, 62, 0.10);
+        padding: 8px;
+        widht: 35px;
+        border-radius: 12.5px;
+        height: 35px;
+        cursor: pointer;
+    }
+    
+     p {
+         font-size: 20px;
+         font-weight: 700;
+         color: #35383E;
+     }
+`
+
+export const TarefaInfo = styled.input`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 410px;
+    padding: 15px 15px 15px 10px;
+    gap: 5px;
+    border: none;
+    outline: none;
+
+    color: rgba(53, 56, 62, 0.50);
+    font-size: 22px;
+    font-weight: 800;
     `
